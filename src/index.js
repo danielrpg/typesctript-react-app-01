671225ffe519959a6d44f0196373b1771bbae92e
Daniel Fernandez <daniel.fernandezgp@gmail.com>
2021-06-21 13:17:21 -0400

    The structure of the project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { render } from "react-dom";
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <h2>This is my website!</h2>
+        </>
+    );
+};
+
+render(<App />, document.body);
